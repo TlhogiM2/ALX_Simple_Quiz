f30c4e3845551af322d740a9564b55d7e35697a1
Letlhogonolo Mathe <tlhogi.tquet@gmail.com>
2024-10-06 17:58:22 +0200

    Add files to the project

---
+++ quiz.js
@@ -0,0 +1,19 @@
+function checkAnswer() {
+    const correctAnswer = "4";
+    const userAnswer = document.querySelector('input[name="quiz"]:checked');
+
+    if (userAnswer) {
+        const feedback = document.getElementById('feedback');
+        if (userAnswer.value === correctAnswer) {
+            feedback.textContent = "Correct! Well done.";
+            feedback.style.color = 'green';
+        } else {
+            feedback.textContent = "That's incorrect. Try again!";
+            feedback.style.color = 'red';
+        }
+    } else {
+        alert('Please select an answer.');
+    }
+}
+
+document.getElementById('submit-answer').addEventListener('click', checkAnswer);
